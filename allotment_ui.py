b320b3ec0940572ee5f587781856be85b4cf86fb
Abhilash-CV <abhilashmayavi@gmail.com>
2025-09-17 12:59:59 +0530

    Create allotment_ui.py

---
+++ allotment_ui.py
@@ -0,0 +1,16 @@
+# allotment_ui.py
+import streamlit as st
+from utils import load_table, download_button_for_df
+
+
+def allotment_ui(year=None, program=None):
+    st.subheader("📜 Allotment")
+    
+    # Load data
+    df_allot = load_table("Allotment", year, program)
+
+    if df_allot.empty:
+        st.info("No allotment data found yet.")
+    else:
+        download_button_for_df(df_allot, f"Allotment_{year}_{program}")
+        st.dataframe(df_allot, use_container_width=True)
