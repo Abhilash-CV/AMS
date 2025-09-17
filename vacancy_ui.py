d6fb818495fbca5d3ac064afcaa8ef2759f84961
Abhilash-CV <abhilashmayavi@gmail.com>
2025-09-17 12:59:08 +0530

    Create vacancy_ui.py

---
+++ vacancy_ui.py
@@ -0,0 +1,7 @@
+# vacancy_ui.py
+import streamlit as st
+
+
+def vacancy_ui(year=None, program=None):
+    st.subheader("📉 Vacancy")
+    st.info("Vacancy calculation will be added later. Upload/edit Seat Matrix and Allotment to prepare for vacancy calculation.")
